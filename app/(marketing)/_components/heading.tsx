347887b6e76e41e978670e8cc436392a04ae889b
Ashi Bandara <ashibandara@Ashis-MacBook-Air.local>
2024-01-29 23:21:34 +0530

    Added the navigation functionality

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+import { ArrowRight } from "lucide-react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
+
+export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-medium">
+      Harmonizing your Ideas, Documents, & Plans <br/> Welcome to the Unified World of <br/> <span className="font-bold">InnoWave</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-regular">
+      Step into the InnoWave Workspace, where seamless connections pave the way for better, faster work...
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get InnoWave for free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  )
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://related-chow-3.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
